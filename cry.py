144f00d0ea54d645e7c99882d0a5f2e9ddb8150e
Shadzino67 <165321524+Shadzino67@users.noreply.github.com>
2025-02-08 13:44:03 +0300

    cry.py

--- cry.py
+++ cry.py
@@ -1,11 +1,12 @@
 from ultralytics import YOLO
+#training of thirds state
 
 # Path to the pretrained model
-model_path = r"C:\Users\alzah\Downloads\model.pt"
+model_path = r"C:\Users\alzah\Downloads\model.pt" #ofc change path
 
 # Load the pretrained model
 model = YOLO(model_path)
 
 # Train the model with the 'crying' data
-model.train(data='data.yaml', epochs=10, imgsz=640, device='cpu')
+model.train(data='data.yaml', epochs=10, imgsz=640, device='cpu') 
 
